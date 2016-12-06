5589fc2086838db39d67ea0d147c4ef5b0e18300
Ethan B <ethanbinya@aol.com>
2016-12-06 14:02:25 -0500

    added constructor and destrustructor

--- WeatherMatrix/Matrix.cpp
+++ WeatherMatrix/Matrix.cpp
@@ -1,12 +1,21 @@
 #include"Matrix.h"
 
-Matrix::Matrix()
+Matrix::Matrix(int l, int h)
 {
-	
+	_length = l;
+	_height = h;
+
+	myArr = new int*[_height];
+
+	for (int i = 0; i < _height; i++)
+		myArr[i] = new int[_length];
 }
 
 Matrix::~Matrix()
 {
-	for(int i=0;i<)
+	for (int i = 0; i < _height; i++)
+		delete[] myArr[_height];
+	delete[] myArr;
 }
+
 
